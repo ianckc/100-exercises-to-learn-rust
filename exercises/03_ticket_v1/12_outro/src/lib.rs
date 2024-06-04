6636c64b5c53e2c1382d5467ff29d68ac90984f1
Ian Luckraft <ian@ianluckraft.co.uk>
2024-06-04 08:12:52 +0100

    Completed (03) ticket_v1 - (12) outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,70 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    pub product_name: String,
+    pub quantity: u32,
+    pub unit_price: u32,
+}
+
+fn is_product_name_valid(product_name: &String) {
+    if product_name.is_empty() {
+        panic!("Product name cannot be empty");
+    }
+    if product_name.len() > 300 {
+        panic!("Product name cannot be longer than 300 bytes");
+    }
+}
+
+fn is_quantity_valid(quantity: &u32) {
+    if quantity == &0 {
+        panic!("Quantity must be greater than zero");
+    }
+}
+
+fn is_unit_price_valid(unit_price: &u32) {
+    if unit_price == &0 {
+        panic!("Unit price must be greater than zero");
+    }
+}
+
+impl Order {
+    // Getters
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+    pub fn total(&self) -> u32 {
+        &self.quantity * &self.unit_price
+    }
+
+    // Setters
+    pub fn set_product_name(&mut self, new_name: String) {
+        is_product_name_valid(&new_name);
+        self.product_name = new_name;
+    }
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        is_quantity_valid(&new_quantity);
+        self.quantity = new_quantity;
+    }
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        is_unit_price_valid(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        is_product_name_valid(&product_name);
+        is_quantity_valid(&quantity);
+        is_unit_price_valid(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+}
